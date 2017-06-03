393a3b53859f2d8bbe15f8ef24d70ffcc9632597
baesparza <bruno.be81@gmail.com>
2017-06-03 18:34:27 -0500

    movie page added

---
+++ src/app/components/movie/movie.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TmdbService } from './../../services/tmdb.service';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styles: []
+})
+export class MovieComponent implements OnInit {
+
+  pelicula: object;
+  ruta: string = '';
+
+  constructor(private _activatedRoute: ActivatedRoute, private _tmdbService: TmdbService, private _router: Router) {
+
+    this._activatedRoute.params.subscribe(param => {
+      // console.log( param );
+      this.ruta = param.pagina;
+      this._tmdbService.get_pelicula( param.id )
+        .subscribe(data => {
+          // console.log(data);
+          this.pelicula = data;
+        });
+    });
+
+  }
+
+  volver_pagina() {
+    if ( this.ruta !== 'mhome') {
+      // console.log('Viene de search');
+      this._router.navigate(['/search', this.ruta]);
+    } else {
+      // console.log('Viene de home');
+      this._router.navigate(['/home']);
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
