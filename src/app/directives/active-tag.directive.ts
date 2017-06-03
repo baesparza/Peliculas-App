3790f24d09c43c31fd7f8923f97dea8c9bc98ee9
baesparza <bruno.be81@gmail.com>
2017-06-02 23:44:04 -0500

    Added styles for the cards

--- src/app/directives/active-tag.directive.ts
+++ src/app/directives/active-tag.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appActiveTag]'
 })
 export class ActiveTagDirective {
 
-  constructor() { }
+  nuevoColor = '#0275d8';
+
+  constructor(private el: ElementRef) {
+    // console.log('Hola');
+  }
+
+  @HostListener('mouseenter') mouseEntro() {
+    // console.log('entro');
+    this.el.nativeElement.style.color  = this.nuevoColor;
+    this.el.nativeElement.style.border = `1px solid ${ this.nuevoColor }`;
+    this.el.nativeElement.style.boxShadow = `0 8px 8px rgba(0,0,0,0.24), 0 0 8px rgba(0,0,0,0.12)`;
+  }
+
+  @HostListener('mouseleave') mouseSalio() {
+    this.el.nativeElement.style.color  = null;
+    this.el.nativeElement.style.border = `1px solid rgba(0,0,0,.125)`;
+    this.el.nativeElement.style.boxShadow = `0 2px 2px rgba(0,0,0,0.24), 0 0 2px rgba(0,0,0,0.12)`;
+  }
+
+  private resaltar (color: string) {
+    
+  }
 
 }
